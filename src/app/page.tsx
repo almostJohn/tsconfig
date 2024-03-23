5f3365fb4505f70da8005199406efee1c73676dd
Jhan Gale Garcia <garcia.johngale@gmail.com>
2024-03-23 21:23:12 +0800

    add: GitHub button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
+import Link from "next/link";
 import { Announcement } from "~/components/announcement";
 import { CopyConfigButton } from "~/components/copy-config-button";
 import { PageActions, PageHeader, PageHeaderDescription, PageHeaderHeading } from "~/components/page-header";
+import { cn } from "~/util/cn";
+import { ArrowUpRightIcon } from "lucide-react";
 
 export default function Page() {
 	return (
@@ -17,8 +20,17 @@ export default function Page() {
 					quality. Copy, paste, and unlock TypeScript&apos;s full potential in your development journey.
 				</PageHeaderDescription>
 				<PageActions>
-					<CopyConfigButton />
+					<Link
+						href="https://github.com/almostJohn/tsconfig"
+						className={cn(
+							"inline-flex px-6 py-2 rounded-md items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-neutral-300 bg-neutral-50 hover:bg-neutral-200 dark:border-neutral-700 dark:bg-transparent dark:hover:bg-neutral-800",
+						)}
+					>
+						GitHub <ArrowUpRightIcon />
+					</Link>
 				</PageActions>
+				<div className="pt-4" />
+				<CopyConfigButton />
 			</PageHeader>
 		</main>
 	);
